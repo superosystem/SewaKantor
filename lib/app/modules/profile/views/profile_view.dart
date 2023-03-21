d63199961c48302acfec51dac6489301c059511c
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-03-21 10:39:33 +0700

    profile screen

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,18 +7,111 @@ import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
   const ProfileView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Get.back(),
         ),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(Icons.logout, color: Colors.black),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                AvatarGlow(
+                  endRadius: 80,
+                  glowColor: Colors.black,
+                  duration: Duration(seconds: 2),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black38,
+                      borderRadius: BorderRadius.circular(100),
+                      image: DecorationImage(
+                          image: AssetImage("assets/logo/noimage.png"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                Text(
+                  "User Name",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "User Email",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Expanded(
+            child: Container(
+              child: Column(
+                children: [
+                  ListTile(
+                    onTap: () => {},
+                    leading: Icon(Icons.note_add_outlined),
+                    title: Text(
+                      "Update Status",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    trailing: Icon(Icons.arrow_right),
+                  ),
+                  ListTile(
+                    onTap: () => {},
+                    leading: Icon(Icons.note_add_outlined),
+                    title: Text(
+                      "Change Profile",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    trailing: Icon(Icons.arrow_right),
+                  ),
+                  ListTile(
+                    onTap: () => {},
+                    leading: Icon(Icons.note_add_outlined),
+                    title: Text(
+                      "Theme",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    trailing: Text("Light"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Chatto",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                Text(
+                  "v1.0.0",
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class ChattpApp extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.done) {
             return GetMaterialApp(
               title: "Chatto",
-              initialRoute: Routes.SEARCH_CONTACT,
+              initialRoute: Routes.PROFILE,
               getPages: AppPages.routes,
             );
             // return FutureBuilder(
