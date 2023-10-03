c7f0bd938ef4fe834b23f1cb0b86303d67b00705
Rara <118464934+darksister76@users.noreply.github.com>
2023-10-03 07:39:49 +0700

    Delete klinikvaksinweb directory

--- klinikvaksinweb/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducers } from "./features";
-
-export const store = configureStore({
-  reducer:rootReducers,
-  devTools: process.env.NODE_ENV === 'development',
-})
